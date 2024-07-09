10fcae4415a195f7defe20f5ee5f08eacc72635a
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2024-07-09 09:53:32 +0300

    init + basic entities

---
+++ MyGarden/MyGarden.Server/Entity/Events/Notification.cs
@@ -0,0 +1,10 @@
+﻿namespace MyGarden.Server.Entity.Events
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.MinValue;
+        public int EventId { get; set; }
+    }
+}
---
+++ MyGarden/MyGarden.Server/Entity/Plant.cs
@@ -0,0 +1,45 @@
+﻿namespace MyGarden.Server.Entity
+{
+    public class Plant
+    {
+        public required int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string BiologyTitle { get; set; } = string.Empty;
+        public WateringNeed Watering { get; set; } = WateringNeed.None;
+        public LightNeed LightNeed { get; set; } = LightNeed.None;
+        public string Fertilization { get; set; } = string.Empty;
+        public string Toxicity { get; set; } = string.Empty;
+        public GrowStage Stage { get; set; } = GrowStage.None;
+        public string Replacing { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int ImageId { get; set; } = 0;
+        public required int GroupId { get; set; }
+        public int RipeningPeriod { get; set; } = int.MaxValue;
+    }
+
+    public enum GrowStage
+    {
+        None,
+        Seed,
+        Sprout,
+        Junior,
+        FruitBearer,
+        Aged
+    }
+
+    public enum LightNeed
+    {
+        None,
+        Low,
+        Medium,
+        High
+    }
+
+    public enum WateringNeed
+    {
+        None,
+        Low,
+        Medium,
+        High
+    }
+}
