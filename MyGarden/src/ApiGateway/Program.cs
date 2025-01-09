3974822038c99798410c2ae3337f0a66d96549b6
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2025-01-10 00:32:04 +0300

    containers healthcheck

--- MyGarden/src/ApiGateway/Program.cs
+++ MyGarden/src/ApiGateway/Program.cs
@@ -3,19 +3,19 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddCustomJwtAuthentication(builder.Configuration);
-builder.Services.AddHealthChecks();
 
 var app = builder.Build();
-await app.UseOcelot();
+app.MapHealthChecks("/healthz");
 
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapHealthChecks("/health");
+await app.UseOcelot();
 
 app.Run();
