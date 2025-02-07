ee4e49910c5d1d514aee42686ac41d1fba2e0821
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2025-02-07 09:56:34 +0300

    data load on initialization

--- MyGarden/src/RecognizeAPI/main.py
+++ MyGarden/src/RecognizeAPI/main.py
@@ -14,26 +14,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+plants_data = get_plants_data()
+
 @app.post("/recognize")
 async def recognize_plant(image: UploadFile = File(...)):
     with open(f"uploads/{image.filename}", "wb") as buffer:
         shutil.copyfileobj(image.file, buffer)
-        
-    plants_data = get_plants_data()
-
     identifiers = identify([f"uploads/{image.filename}"],plants_data,top_number=3)
     for index,id in enumerate(identifiers):
         print(f"ФОТО {index+1} ({id[0]})")
         print('\n'.join([f"\t{x[0]:<50}\t\t{x[1]}" for x in id[1]]))
-
     result = {"filename": image.filename, "message": '\n'.join([f"\t{x[0]:<50}\t\t{x[1]}" for x in id[1]]), "species": "Example Species"}
     
     return JSONResponse(content=result)
 
 @app.get("/search")
 async def search_plant(name: str = Query(...)):
-    
-    plants_data = get_plants_data()
     result=[x for x in plants_data if name.lower() in ((x['title']).lower())]
     return JSONResponse(content=result)
     
